78dc1770e0f22f75789ff5a50ca48f7d1b43e9d5
fujiwaraX <mendelssohn4116@outlook.jp>
2022-04-24 09:22:58 +0900

    add_timer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="app">
+    <p>{{time}}</p>
+    <button @click="recalc()">開始</button>
+    <br />
+    <br />
     <button @click="addGlass()">追加する</button>
     <div class="glass_outer">
       <div class="glass_inner" :style="{background:'lightblue', height: `${computedGlassHeight}px`, 'top': `${computedLiquidTop}px`}"></div>
@@ -18,7 +22,8 @@ export default {
   data() {
     return {
       glass: 0,
-      ADD_UNIT: 10
+      ADD_UNIT: 10,
+      time: 0
     }
   },
   computed:{
@@ -32,6 +37,32 @@ export default {
   methods: {
     addGlass() {
       this.glass = this.glass + 1
+    },
+    countdown(finish_time){
+      const now=new Date();
+      const rest=finish_time-now.getTime();
+      const sec=Math.floor(rest/1000)%60;
+      const min=Math.floor(rest/1000/60)%60;
+      const rest_time=[min, sec];
+      return rest_time;
+    },
+    recalc(){
+      const time_limit=32;
+      this.time=`残り 0:30`;
+      const start_time=new Date();
+      const finish_time=start_time.getTime()+time_limit*1000;
+      const self=this;
+      setInterval(
+          function(){
+              console.log(`aaa`);
+              const timer=self.countdown(finish_time);
+              self.time=`残り ${timer[0]}:${timer[1]}`;
+              if (timer[1]==0) {
+                self.time=`終了`;
+              }
+          },
+          1000
+      )
     }
   }
 }
